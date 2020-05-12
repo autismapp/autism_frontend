5ace0a8f2e42135026fb6bcaba5e58b4581dc844
Jacanroman <jacanroman@gmail.com>
2020-05-12 18:17:55 +0100

    link working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 import React from 'react';
 import './App.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import FeelingsMenu from './components/FeelingsMenu/FeelingsMenu'
 import Schedule from './components/Schedule/Schedule'
-import AngryFeeling from './components/AngryFeeling/AngryFeeling'
-import Navbar from './components/Navbar'
+import Home from './components/Home/Home'
+import mood from './images/mood.svg'
+import wait from './images/wait.svg'
+import logo from './images/Frame1.png'
+import Navbar from './components/Navbar';
+import {Route, Link, Switch} from 'react-router-dom';
+
+/*
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <h1>First Change</h1>
-      <h2>this is the first branch</h2>
       
-      <FeelingsMenu />
-      <Schedule />
-      <AngryFeeling />
+      <Navbar />
 
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/Schedule" component={Schedule}/>
+        <Route path="/FeelingsMenu" component={FeelingsMenu} />
+      </Switch>
 
+      
+     
+      <div className="main">
+        <h1>Hey! Welcome to the Autism Scheduler App</h1>
+        <h2>Click on the images below to tell us how you're feeling today. Or how about updating your schedule.</h2>
+
+        <a href = {<FeelingsMenu />}><img src={mood} alt="mood" /></a> 
+        <a href = { <Schedule />}><img src={wait} alt="wait" /></a> 
+        
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
+
+*/
+
+function App(){
+  return (
+    <div className="App">
+      
+      <Navbar />
+
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/Schedule" component={Schedule}/>
+        <Route path="/FeelingsMenu" component={FeelingsMenu} />
+      </Switch>
+      
+    
+    </div>
+  );
+}
+
+export default App;
+
+
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,35 @@
 import React from 'react';
-import Schedule from './components/Schedule/Schedule'
-import FeelingsMenu from './components/FeelingsMenu/FeelingsMenu'
-import mood from './images/mood.svg'
-import wait from './images/wait.svg'
 
+//import Schedule from './components/Schedule/Schedule'
+//import FeelingsMenu from './components/FeelingsMenu/FeelingsMenu'
+//import mood from '../images/mood.svg'
+//import wait from './images/wait.svg'
+import mood from '../Home/images/mood.svg';
+import wait from '../Home/images/wait.svg';
+import { NavLink } from 'react-router-dom';
+
+function Home(){
+    return (
+        <div className="main">
+            <h1>Hey! Welcome to the Autism Scheduler App</h1>
+            <h2>Click on the images below to tell us how you're feeling today.</h2> 
+            <h3>Or how about updating your schedule.</h3>
+            <NavLink to="/FeelingsMenu">
+            <a href = ""><img src={mood} alt="mood" /></a>
+            </NavLink> 
+            <NavLink to="/schedule">
+            <a href = ""><img src={wait} alt="wait" /></a>
+            </NavLink> 
+            
+        </div>
+    )  
+}
+
+
+export default Home
+
+
+/*
 function Home() {
     return (
         <div className="main">
@@ -19,3 +45,4 @@ function Home() {
 
 
 export default Home;
+*/
